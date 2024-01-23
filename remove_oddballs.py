1b973b774542da37eabc45dd8ee3c413d2eeff2e
Steven <stevezzhang@gmail.com>
2024-01-23 05:38:20 -1000

    Remove rows with first col 252

--- remove_oddballs.py
+++ remove_oddballs.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Define the file paths
-input_file_path = 'eeg_data/after/sub-01/ses-01/sub-01_ses-01_preletswave.csv'
-output_file_path = 'eeg_data/after/sub-01/ses-01/sub-01_ses-01_preletswave.csv'
+input_file_path = 'eeg_data/after/sub-01/ses-01/sub-06_ses-01_preletswave.csv'
+output_file_path = 'eeg_data/after/sub-01/ses-01/modified_sub-06_ses-01_preletswave.csv'
 
 # Load the CSV file
 df = pd.read_csv(input_file_path)
@@ -11,15 +11,13 @@ df = pd.read_csv(input_file_path)
 rows_to_delete = []
 
 # Iterate through the DataFrame
-for i in range(1, len(df)):
-    if df.at[i, 'code'] in [254, 251]:
-        rows_to_delete.extend([i-1, i])
-
-# Remove duplicates from the list
-rows_to_delete = list(set(rows_to_delete))
-
-# Debugging: Print the rows to be deleted
-print(f"Rows to delete: {rows_to_delete}")
+for i in range(len(df) - 1):
+    # Check if the current row's 'code' is 252
+    if df.at[i, 'code'] == 252:
+        rows_to_delete.append(i)
+    # Check if the next row's 'code' is not 252 or 253
+    elif df.at[i + 1, 'code'] not in [252, 253]:
+        rows_to_delete.append(i)
 
 # Drop the rows
 df.drop(rows_to_delete, inplace=True)
