aff2ae82ee9489f0f989b430dd0d4222ce361db1
LemonFace0309 <charles.liu0309@gmail.com>
2024-01-24 20:41:55 -0500

    replaced old import csv with new script

--- import_csv_to_letswave.m
+++
@@ -1,8 +0,0 @@
-% Read the table from the CSV file, change #
-T = readtable('sub-0#_ses-0#_preletswave.csv')
-
-% Directly create a cell array from the selected columns of the table
-events = table2cell(T(:, 1:3));
-
-% Clear all variables except 'events'
-clearvars -except events
