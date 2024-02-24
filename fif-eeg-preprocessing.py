cf19a5ab71c3c9045527f3cd440e6c921d5a1308
LemonFace0309 <charles.liu0309@gmail.com>
2024-02-24 00:08:22 -0800

    cleaned up some path info

--- fif-eeg-preprocessing.py
+++ fif-eeg-preprocessing.py
@@ -6,15 +6,13 @@ import mne
 from mne.preprocessing import ICA
 import os
 import scipy.signal
-import zipfile
 import numpy as np 
 
-# Setup directories
-output_dir = 'final_eeg_files'
-os.makedirs(output_dir, exist_ok=True)
+
+input_file = 'subj04_session2.fif'
 
 # Load the BDF file
-fif_file_path = 'create_final_dataset/subj04_session2.fif'  
+fif_file_path = os.path.join('eeg_data', 'fif', input_file) 
 input_file_name = os.path.basename(fif_file_path)
 raw = mne.io.read_raw_fif(fif_file_path, preload=True)
 print(raw._data.shape)
@@ -55,7 +53,8 @@ epochs.set_eeg_reference('average')
 epochs.apply_baseline(baseline=(-0.05, 0))
 
 # Saving the preprocessed data
-preprocessed_file_path = os.path.join(output_dir, input_file_name)
+preprocessed_file_path = fif_file_path.replace('fif', 'final_eeg_files')
+os.makedirs(os.path.dirname(preprocessed_file_path), exist_ok=True)
 epochs.save(preprocessed_file_path)
 
 # Zipping the preprocessed data (Step 20)
