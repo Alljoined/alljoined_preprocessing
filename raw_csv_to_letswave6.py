0b3e8fc988d2fe710b65ae4cbfba70bddb30f80b
Kenson Hui <kenson.hui22@gmail.com>
2024-02-23 20:13:35 -0500

    Fix indexing bug in letswave6 final data script

--- raw_csv_to_letswave6.py
+++ raw_csv_to_letswave6.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+""" 
+This script is responsible for building the behavioural data
+    portion of the final dataset in the Post-LetsWave6 format
+"""
+
 Post_letsWave6_columns = [
     "subject_id",
     "session",
@@ -12,10 +17,10 @@ Post_letsWave6_columns = [
 
 EVENT_CODES = {
     "BLOCK_START": 65536,
-    "CORRECT_REJ": 252, # Not oddball, no press
+    "CORRECT_REJ": 252, # Not oddball, not pressed
+    "FALSE_ALARM": 253, # Not oddball, pressed
     "CORRECT_HIT": 254, # Oddball, pressed
-    "FALSE_ALARM": 253, # Not oddball, press
-    "MISS": 251,
+    "MISS": 251,        # Oddball, not pressed
     "ODDBALL_START": 151, # Oddball codes go from 151 to 166
     "ODDBALL_END": 166
 }
@@ -23,21 +28,38 @@ EVENT_CODES = {
 def log_error(message, row, trial):
     print(f"Error: {message} occured in row {row}, corresponding trial: {trial}")
 
-def convert_raw_eeg_csv_to_letswave6(
-        subject_id,
-        session,
+def convert_raw_eeg_to_letswave6(
+        subject_id: int,
+        session: int,
         raw_eeg_df: pd.DataFrame,
         sample_rate: int = 512
         ) -> pd.DataFrame:
     """
-    Converts the raw_eeg_csv form of [onset, sig, code] 
-        to the Post_LetsWave6 Format
+    Converts raw EEG data to the Post_LetsWave6 Format
+    
+    Parameters:
+    - subject_id: subject id
+    - session: the number of the session
+    - raw_eeg_df: EEG data with the columns onset, sig, code.
+    - sample_rate: sample rate of the EEG data
+
+    Returns:
+    - DataFrame with columns = [
+        "subject_id",
+        "session",
+        "block",
+        "curr_time",
+        "is_oddball",
+        "pressed",
+        "reaction_time"
+      ]
     """
     # We skip two rows, one is start signal, second is block start
     inital_rows_to_skip = 2 
     rows_per_trial = 3
     output = pd.DataFrame(columns=Post_letsWave6_columns)
     block = 1
+    num_trials = 0
 
     for i in range(inital_rows_to_skip, len(raw_eeg_df), rows_per_trial):
         is_oddball = False
@@ -66,6 +88,7 @@ def convert_raw_eeg_csv_to_letswave6(
             continue
         elif trial_block_code >= EVENT_CODES["ODDBALL_START"] and \
                 trial_block_code <= EVENT_CODES["ODDBALL_END"]:
+            # Oddball event
             is_oddball = True
             if trial_user_response_code == EVENT_CODES["CORRECT_HIT"]:
                 pressed = True
@@ -82,13 +105,14 @@ def convert_raw_eeg_csv_to_letswave6(
             "pressed": pressed,
             "reaction_time": reaction_time
         }
-        output.loc[(i - inital_rows_to_skip) // rows_per_trial] = new_entry
+        output.loc[num_trials] = new_entry
+        num_trials += 1
     
     return output
 
 if __name__ == "__main__":
     raw_eeg_df = pd.read_csv("tests/subj04_session2.csv")
-    eeg_letswave6 = convert_raw_eeg_csv_to_letswave6(
+    eeg_letswave6 = convert_raw_eeg_to_letswave6(
         subject_id=4,
         session=2,
         raw_eeg_df=raw_eeg_df
