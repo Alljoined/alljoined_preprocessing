527ee2b386b54008ef91a172b52f35606ae82603
Jonathan <jonathanxu25@gmail.com>
2024-03-03 18:36:05 -0500

    storing as hdf5 file instead of csv to avoid eeg getting ellipses

--- final_dataset/create_huggingface_dataset.py
+++ final_dataset/create_huggingface_dataset.py
@@ -3,14 +3,16 @@ import h5py
 import numpy as np
 from PIL import Image
 import pandas as pd
-from datasets import Dataset, load_dataset
+from datasets import Features, Dataset, Sequence, Value, Image as DatasetsImage
 from dotenv import load_dotenv
+import re
+
 
 DSET_NAME = "05_125"
 load_dotenv()
 HF_PUSH = os.getenv("HF_PUSH")
 
-DATASET_PATH = '../eeg_data/combined_dataset.csv'
+DATASET_PATH = '../eeg_data/combined_dataset.h5'
 COCO_PATH = 'stimulus/datasets--pscotti--mindeyev2/snapshots/183269ab73b49d2fa10b5bfe077194992934e4e6/coco_images_224_float16.hdf5'
 
 def fetch_image(nsd_id, file_path=COCO_PATH):
@@ -24,7 +26,7 @@ def generate_hf_dataset(df, file_path=COCO_PATH):
     for _, row in df.iterrows():
         image = fetch_image(row['73k_id'], file_path)
         yield {
-            'EEG': row['eeg'], 
+            'EEG': row["eeg"], 
             'image': image,
             'subject_id': row['subject_id'],
             'session': row['session'],
@@ -35,8 +37,11 @@ def generate_hf_dataset(df, file_path=COCO_PATH):
             'curr_time': row['curr_time'],
         }
 
-df = pd.read_csv(DATASET_PATH)
+        break
+
+df = pd.read_hdf(DATASET_PATH, key="df")
 
 print("Creating hf dataset")
 hf_dataset = Dataset.from_generator(generator=generate_hf_dataset, gen_kwargs={"df": df}, cache_dir="huggingface")
+
 hf_dataset.push_to_hub("Alljoined/" + DSET_NAME, token=HF_PUSH)
